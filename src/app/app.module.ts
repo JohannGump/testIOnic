c88564f95a583f1a916fee2126996d5ca80da5be
Johann <johann@macbook-pro-de-johann.home>
2018-02-18 18:54:20 +0100

    test 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
+
+// Pages
 import { HomePage } from '../pages/home/home';
+import { DetailsPage } from '../pages/details/details';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailsPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+// Pages
+import { DetailsPage } from '../details/details';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  nom: string;
+  prenom: string;
+  age: number;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  private showDetails(){
+    console.log(this.nom);
+    console.log(this.prenom);
+    console.log(this.age);
+    //this.navCtrl.push(DetailsPage);
   }
 
 }
